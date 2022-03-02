fd08c9a64f5b582b0ed9c0e17edeed0bbf08061b
Théo Choffat <theo.choffat@hes-so.ch>
2022-03-02 17:57:26 +0100

    Menu part 1

--- app/src/main/java/com/example/tennisapplication/SummaryActivity.java
+++ app/src/main/java/com/example/tennisapplication/SummaryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -16,13 +17,26 @@ public class SummaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_summary);
 
         MaterialButton toolbarButton = (MaterialButton) findViewById(R.id.toolbaraccountbutton);
-
         toolbarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openAccountActivity();
             }
         });
+
+        // Create menu Button
+        Button menuBtn = (Button) findViewById(R.id.menubutton);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuActivity();
+            }
+        });
+    }
+
+    private void openMenuActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 
     private void openAccountActivity(){
