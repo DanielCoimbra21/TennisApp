0da7afd763fbc8a8773faf6ffba87bd5e27a240b
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-03-02 17:33:02 +0100

    Menu

---
+++ app/src/main/java/com/example/tennisapplication/MenuActivity.java
@@ -0,0 +1,52 @@
+package com.example.tennisapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        ListView listView = findViewById(R.id.listview);
+
+        List<String> list = new ArrayList<>();
+        list.add("Account");
+        list.add("sign-Out");
+        list.add("Banana");
+        list.add("Theo I LUV U");
+
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    //clicked Account
+
+                    startActivity(new Intent(MenuActivity.this,AccountActivity.class));
+
+                }else if(position==1){
+                    //clicked SignOut
+                    startActivity(new Intent(MenuActivity.this, MainActivity.class));
+                }else{
+
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/tennisapplication/SummaryActivity.java
+++ app/src/main/java/com/example/tennisapplication/SummaryActivity.java
@@ -1,10 +1,16 @@
 package com.example.tennisapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -16,17 +22,32 @@ public class SummaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_summary);
 
         MaterialButton toolbarButton = (MaterialButton) findViewById(R.id.toolbaraccountbutton);
-
         toolbarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openAccountActivity();
             }
         });
+
+        // Create menu Button
+        Button menuBtn = (Button) findViewById(R.id.menubutton);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenuActivity();
+            }
+        });
+    }
+
+    private void openMenuActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 
     private void openAccountActivity(){
         Intent intent = new Intent(this, AccountActivity.class);
         startActivity(intent);
     }
+
+
 }
